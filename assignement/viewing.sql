2523aa805d1b2e8c4bf9a4ab91b236cfff461ada
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-10 00:02:19 +0200

    added viewing table as instructed in a lab assignement

--- assignement/viewing.sql
+++ assignement/viewing.sql
@@ -1,1 +1,17 @@
-CREATE TABLE propert
+CREATE TABLE Viewing(
+    clientNo VARCHAR(5) PRIMARY KEY,
+    propertyNo VARCHAR(6),
+    viewData DATE,
+    comment VARCHAR(20), 
+    FOREIGN KEY(propertyNo) REFERENCES PopertForRent(propertyNo)
+);
+
+INSERT INTO Viewing(
+    clientNo, propertyNo, viewData, comment
+)
+VALUES
+    ("CR56", "PA14", "2004-05-24", "too small"),
+    ("CR76", "PG4", "2004-04-20", "too remote"),
+    ("CR56", "PG4", "2004-05-26"),
+    ("CR62", "PA14", "2004-05-14", "no dining room"),
+    ("CR56", "PG36", "2004-04-28");
