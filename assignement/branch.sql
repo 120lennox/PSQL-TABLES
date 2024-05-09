71206e9bbfa5e0b70db171e719888fe7146e5f1a
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-10 00:21:17 +0200

    fixed branch errors with quotes and removed redundancies

--- assignement/branch.sql
+++ assignement/branch.sql
@@ -1,17 +1,14 @@
-CREATE TABLE Branch(
-    BranchNO VARCHAR(5) PRIMARY KEY,
+CREATE TABLE Branch (
+    BranchNo VARCHAR(5) PRIMARY KEY,
     street VARCHAR(20),
     City VARCHAR(20),
-    Postcode VARCHAR(30),
-    UNIQUE (BranchNo)
+    Postcode VARCHAR(30)
 );
 
-INSERT INTO (
-    BranchNo, street, City, Postcode
-)
-VALUES 
-    ("B005", "22 Deer Rd", "London", "SW1 4EH"),
-    ("B007", "16 Argyll St", "Aberdeen", "AB2 3SU"),
-    ("B003", "163 Main St", "Glasgow", "G11 9QX"),
-    ("B004", "32 Manse Rd", "Bristol", "BS99 1NZ"),
-    ("B002", "56 CLOVER Dr", "London", "NW10 6EU");
+INSERT INTO Branch (BranchNo, street, City, Postcode)
+VALUES
+    ('B005', '22 Deer Rd', 'London', 'SW1 4EH'),
+    ('B007', '16 Argyll St', 'Aberdeen', 'AB2 3SU'),
+    ('B003', '163 Main St', 'Glasgow', 'G11 9QX'),
+    ('B004', '32 Manse Rd', 'Bristol', 'BS99 1NZ'),
+    ('B002', '56 CLOVER Dr', 'London', 'NW10 6EU');
